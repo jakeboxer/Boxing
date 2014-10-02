01cc9160d528edb372acc10db41206ef2e402f4e
Jake Boxer <jake@github.com>
2014-10-01 23:36:27 -0700

    Add PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public const int IDLE = 0;
+	public const int BLOCKING = 1;
+	public const int PUNCHING = 2;
+
+	public int leftHandState = IDLE;
+	public int rightHandState = IDLE;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("'")) {
+			rightHandState = PUNCHING;
+		} else if (Input.GetKey("/")) {
+			rightHandState = BLOCKING;
+		} else {
+			rightHandState = IDLE;
+		}
+	}
+}
