9274e4239c437de9ce06324436a329260fd57da5
Jake Boxer <jake@github.com>
2014-10-02 00:17:07 -0700

    Move position when punching/idle

--- Assets/Scripts/Fist.cs
+++ Assets/Scripts/Fist.cs
@@ -8,19 +8,29 @@ public class Fist : MonoBehaviour {
 
 	private Animator animator;
 	private PlayerController controller;
+	private Vector2 idlePosition;
+	private Vector2 punchingPosition;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
 		controller = GetComponent<PlayerController>();
+
+		idlePosition = transform.position;
+
+		float punchingX = idlePosition.x + (fistSide == FistSide.Left ? 1.5f : -1.5f);
+		float punchingY = idlePosition.y + 3f;
+		punchingPosition = new Vector2(punchingX, punchingY);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (GetControlState() == PlayerController.ControlState.Punch) {
 			animator.SetInteger("AnimState", 2);
+			transform.position = punchingPosition;
 		} else {
 			animator.SetInteger("AnimState", 0);
+			transform.position = idlePosition;
 		}
 	}
 
