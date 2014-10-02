740d3083823e81a3add5eaaf8df26417361e82bc
Jake Boxer <jake@github.com>
2014-10-01 23:51:09 -0700

    Make punching animation show on punch

---
+++ Assets/Scripts/Fist.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fist : MonoBehaviour {
+	private Animator animator;
+	private PlayerController controller;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		controller = GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller.rightHandState == PlayerController.PUNCHING) {
+			animator.SetInteger("AnimState", 1);
+		}
+	}
+}
