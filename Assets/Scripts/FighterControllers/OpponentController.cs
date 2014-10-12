6a47094f28179e560567e16744268d6d19086f40
Jake Boxer <jake@github.com>
2014-10-12 00:30:02 -0700

    Add blue glove animations

---
+++ Assets/Scripts/FighterControllers/OpponentController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpponentController : FighterController {
+	public float speed = 2f;
+
+	private ControlState lastLeftControlState;
+	private ControlState lastRightControlState;
+	private float timeSinceLastControlStateChange = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastControlStateChange += Time.deltaTime;
+
+		if (timeSinceLastControlStateChange >= speed) {
+			timeSinceLastControlStateChange = 0f;
+			SwapPunchingHands();
+		}
+	}
+
+	private void SwapPunchingHands () {
+		if (leftControlState == ControlState.Idle) {
+			leftControlState = ControlState.Punch;
+			rightControlState = ControlState.Idle;
+		} else {
+			leftControlState = ControlState.Idle;
+			rightControlState = ControlState.Punch;
+		}
+	}
+}
