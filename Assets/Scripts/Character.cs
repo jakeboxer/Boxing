1cc480d6e4bb3797212731c11e224960d6dd2973
Jake Boxer <jake@github.com>
2014-10-11 19:16:04 -0700

    Add Character script

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	public float maxHealth = 100f;
+	public float health {
+		get;
+		private set;
+	}
+	public bool isDead {
+		get;
+		private set;
+	}
+	public GameObject takeDamageEffect;
+
+	void Awake () {
+		health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void Die () {
+		isDead = true;
+		health = 0f;
+	}
+
+	public void Respawn () {
+		isDead = false;
+		health = maxHealth;
+	}
+
+	public void TakeDamage (float damage) {
+		if (takeDamageEffect) {
+			Instantiate(takeDamageEffect, transform.position, transform.rotation);
+		}
+
+		health -= damage;
+
+		if (health <= 0f) {
+			Die();
+		}
+	}
+}
