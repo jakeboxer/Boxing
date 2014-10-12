ffb5bc3be7b0b725240bb76cbd3a0ba09d3df6a9
Jake Boxer <jake@github.com>
2014-10-11 22:11:50 -0700

    Add FollowObject script

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+	public Vector2 offset;
+	public Transform following;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = following.transform.position + (Vector3) offset;
+	}
+}
