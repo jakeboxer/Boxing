ed792abbb4daeccb4035fb6f786ec69137b10ace
Jake Boxer <jake@github.com>
2014-10-11 23:17:40 -0700

    Automatically calculate label position

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,7 +7,6 @@ public class HealthBar : MonoBehaviour {
 	public SpriteRenderer foregroundRenderer;
 	public Color maxHealthColor = new Color(255f / 255f, 63f / 255f, 63f / 255f);
 	public Color minHealthColor = new Color(64f / 255f, 137f / 255f, 255f / 255f);
-	public Vector2 labelPosition = new Vector2(0f, 0f);
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,8 +18,15 @@ public class HealthBar : MonoBehaviour {
 
 	void OnGUI () {
 		if (character.displayName != null) {
+			float xPosition;
+			if (character.name == "Player") {
+				xPosition = 40f;
+			} else {
+				xPosition = Screen.width - 100f;
+			}
+
 			GUI.Label(
-				new Rect(labelPosition.x, labelPosition.y, 100, 20),
+				new Rect(xPosition, 20f, 100f, 20f),
 				character.displayName
 			);
 		}
