c493c978e8516fad85ac50ba35076eb7f8b7a13f
Jake Boxer <jake@github.com>
2014-10-11 19:27:50 -0700

    Add HealthBar script

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+	public Character character;
+	public Transform foregroundSprite;
+	public SpriteRenderer foregroundRenderer;
+	public Color maxHealthColor = new Color(255f / 255f, 63f / 255f, 63f / 255f);
+	public Color minHealthColor = new Color(64f / 255f, 137f / 255f, 255f / 255f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var healthPercent = character.health / character.maxHealth;
+
+		foregroundSprite.localScale = new Vector3(healthPercent, 1f, 1f);
+		foregroundRenderer.color = Color.Lerp(maxHealthColor, minHealthColor, healthPercent);
+	}
+}
