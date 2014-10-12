9665367c819a542f50b235e04f1a96f05364efe3
Jake Boxer <jake@github.com>
2014-10-12 01:22:00 -0700

    Add abstract IntervalOpponentController class

---
+++ Assets/Scripts/FighterControllers/IntervalOpponentController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class IntervalOpponentController : FighterController {
+	public float speed = 1f;
+	
+	private float timeSinceLastControlStateChange = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastControlStateChange += Time.deltaTime;
+		
+		if (timeSinceLastControlStateChange >= speed) {
+			timeSinceLastControlStateChange = 0f;
+			IntervalPassed();
+		}
+	}
+
+	public abstract void IntervalPassed ();
+}
---
+++ Assets/Scripts/FighterControllers/PunchingIntervalOpponentController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PunchingIntervalOpponentController : IntervalOpponentController {
+
+	public override void IntervalPassed () {
+		if (leftControlState == ControlState.Idle) {
+			leftControlState = ControlState.Punch;
+			rightControlState = ControlState.Idle;
+		} else {
+			leftControlState = ControlState.Idle;
+			rightControlState = ControlState.Punch;
+		}
+	}
+}
